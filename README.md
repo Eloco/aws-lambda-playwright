bc917d434f3d19770e551cb71d1000057827071e
eloco <exia@isimarkets.com>
2022-04-23 16:48:41 +0800

    modify

--- README.md
+++ README.md
@@ -15,13 +15,12 @@ sudo docker run --rm=True -p 9000:8080 ghcr.io/eloco/aws-lambda-playwright
 >https://github.com/microsoft/playwright/blob/main/packages/playwright-core/src/server/deviceDescriptorsSource.json
 ```
 bs64=`echo "stealth_sync(page);page.goto('http://whatsmyuseragent.org/',wait_until='commit'); result=page.content()" | base64 -w 0`
-curl -s -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"webkit":"iPhone SE","run":"'${bs64}'"}' | jq .body | html2text | sed  's/[\\n" ]//g' | grep -v '^$'
-curl -s -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"webkit":"random","run":"'${bs64}'"}' | jq .body | html2text | sed  's/[\\n" ]//g' | grep -v '^$'
+curl -s -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"webkit":"iPhone SE","run":"'${bs64}'"}' | jq .result  | html2text -utf8  | sed -r "s/\\\n//g"  | grep -v '^\s*$' | grep -v '^"'
+curl -s -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"webkit":"random","run":"'${bs64}'"}' | jq .result  | html2text -utf8  | sed -r "s/\\\n//g"  | grep -v '^\s*$' | grep -v '^"'
 
-__[__What'smyUserAget?](#page-top)
-*[](#page-top)
-Mozilla/5.0(iPad;CPUOS12_2likeMacOSX)AppleWebKit/605.1.15(KHTML,
-likeGecko)Versio/15.4Mobile/15E148Safari/604.1
-MyIPAddress:36.161.237.194
-Copyright©What'smyUserAget2015
+ What's my User Agent?
+Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38
+(KHTML, like Gecko) Version/15.4 Mobile/15A372 Safari/604.1
+My IP Address: 34.251.234.37
+Copyright © What's my User Agent 2015
 ```
--- app/app.py
+++ app/app.py
@@ -11,32 +11,43 @@ import httpx,requests
 result = """ u need to send param  event[run] with base64 encode """
 
 def handler(event, context):
-    with sync_playwright() as playwright:
-        global result
-        if "webkit" in event:
-            browser = playwright.webkit.launch(headless=True)
-            if   event["webkit"] in playwright.devices:
+    status_code = 200
+    try:
+        with sync_playwright() as playwright:
+            global result
+            if "webkit" in event:
+                browser = playwright.webkit.launch(headless=True)
                 device_name = event["webkit"]
+                if device_name.lower() == "random":
+                    device_name = random.choice(list(playwright.devices.keys()))
+                else:
+                    for d in playwright.devices.keys():
+                        if device_name.lower() == d.lower():
+                            device_name = d
+                            break
+                    if device_name not in playwright.devices.keys():
+                        device_name = "Desktop Firefox"  # use default
+
+                print(f"webkit using [{device_name}]")
                 device      = playwright.devices[device_name]
                 context     = browser.new_context(**device,)
-            elif event["webkit"] == "random":
-                device_name = random.choice(list(playwright.devices.keys()))
-                device      = playwright.devices[device_name]
-                context     = browser.new_context(**device,)
-            else:
-                context = browser.new_context()
-            page = context.new_page()
+                page = context.new_page()
 
-        run="global result;"
-        if "run" in event.keys():
-            try:
-                run+=base64.b64decode(event["run"]).decode('utf-8')
-            except:
-                run+=event["run"]
+            run="global result;"
+            if "run" in event.keys():
+                try:
+                    run+=base64.b64decode(event["run"]).decode('utf-8')
+                except:
+                    run+=event["run"]
+            print(run)
+            exec(run)
 
-        exec(run)
+    except Exception as e:
+        result=str(e)
+        traceback.print_exc()
+        status_code = 500
 
-        return {
-            'statusCode': 200,
-            'body': result,
-        }
+    return {
+        'code'  : status_code,
+        'result': result,
+    }
