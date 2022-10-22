b98ef7e31af55bdce5f8b1a73863dbf1f43606a3
Elogicocal <22910374+Eloco@users.noreply.github.com>
2022-10-23 01:43:44 +0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -14,10 +14,8 @@ import importlib.machinery
 def handler(event, context):
     status_code = 200
     script_dir = os.path.dirname(os.path.abspath(__file__))
-    
-
+    template_file="async_template.py"
     result="Hello AWS LAMBDA!"
-    print(r"init",result)
 
     if_stealth   = bool(event.get('stealth', False))
     if_reindent  = bool(event.get('reindent', True))
@@ -25,7 +23,7 @@ def handler(event, context):
     device_name  = str(event.get('device', '')).strip().lower()
     run_code     = str(event.get('run', f'result="{result}"')).strip()
 
-    template_file="async_template.py"
+    
 
     try:
         run_code=base64.b64decode(run_code).decode('utf-8')
@@ -36,19 +34,21 @@ def handler(event, context):
         try:
             template=open(os.path.join(script_dir,template_file)).read()
             template=template.replace('<run_code>',run_code)
-            file_path=os.path.join(tmpdirname,"dynamic.py")
-            with open(file_path,"w") as f:
+            dynamic_path=os.path.join(tmpdirname,"dynamic.py")
+            with open(dynamic_path,"w") as f:
                 f.write(template)
                 del template
-            if if_reindent:os.system(f"python -m reindent {file_path} &> /dev/null; \
-                        python -m autopep8 {file_path} --select=E121,E101,E11 --in-place &> /dev/null; \
-                        cat {file_path}")
+            if if_reindent:os.system(f"python -m reindent {dynamic_path} &> /dev/null; \
+                        python -m autopep8 {dynamic_path} --select=E121,E101,E11 --in-place &> /dev/null; \
+                        cat {dynamic_path}")
 
-            loader = importlib.machinery.SourceFileLoader('dynamic', file_path)
+            loader = importlib.machinery.SourceFileLoader('dynamic', dynamic_path)
             handle = loader.load_module('dynamic')
             loop = asyncio.get_event_loop()
             result=loop.run_until_complete( \
-                    handle.main(browser_name=browser_name,if_stealth=if_stealth,device_name=device_name))
+                    handle.main(browser_name=browser_name,\
+                                if_stealth=if_stealth,\
+                                device_name=device_name))
 
         except Exception as e:
             result=traceback.format_exc()
